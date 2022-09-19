9994fc2843f5ba17844d1a100650940924fc37c2
furqonfadlillah7 <78301902+furqonfadlillah7@users.noreply.github.com>
2022-09-19 11:29:42 +0700

    Add files via upload

---
+++ praktikum-nodejs/hello.js
@@ -0,0 +1,24 @@
+//var umur = 100;
+//var nama = "Muhammad Furqon Fadlilah";
+
+//console.log(nama);
+//console.log(`umur ${nama} adalah ${umur} tahun`);
+
+var human = [{
+		nama : "Muhammad",
+		umur : 100,
+		hobi : "menulis",
+		wargaIndonesia: true,
+	},{
+		nama : "furqonnn",
+		umur : 200,
+		hobi : "membaca",
+		wargaIndonesia: true,
+	},{
+		nama : "Fadlilah",
+		umur : 300,
+		hobi : "membaca",
+		wargaIndonesia: true,
+	}];
+
+console.log(human);	
